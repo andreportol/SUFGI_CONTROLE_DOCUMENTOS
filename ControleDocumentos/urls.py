b76b168063e1d41d5586258c64d0ccf14a996a8c
andreportol <andreportol@gmail.com>
2024-08-15 16:00:57 -0400

    Alterada a aplicação para Documentos, visando melhorar a estética do painel do admin.

--- ControleDocumentos/urls.py
+++ ControleDocumentos/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard_documentos.urls',namespace='dashboard_documentos'))
+    path('', include('Documentos.urls',namespace='Documentos'))
 ]
--- Documentos/urls.py
+++ Documentos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import IndexTemplateView
 
-app_name = 'dashboard_documentos'
+app_name = 'Documentos'
 
 urlpatterns = [
     path('',IndexTemplateView.as_view(), name='index')
