fac5d469fcf9a5bd766219b2e4de9a47cb89d335
andreportol <andreportol@gmail.com>
2024-08-15 16:14:48 -0400

    acrescentado o atributo verbose_name para alterar o nome no painel admin do django.

--- Documentos/apps.py
+++ Documentos/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DashboardDocumentosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Documentos'
+    # Configura o painel admin do django para aparecer esse nome no lugar do nome da aplicação
+    verbose_name = 'Administração'
